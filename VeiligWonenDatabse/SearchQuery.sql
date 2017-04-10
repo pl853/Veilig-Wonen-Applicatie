bd76c9f99d5b71a08f272bab6b18afdc161e4747
pieterLems <Pieter1212112@gmail.com>
2017-04-10 14:30:56 +0200

    m

--- VeiligWonenDatabse/SearchQuery.sql
+++
@@ -1,5 +0,0 @@
-﻿select StadsGebied.Naam as Gebied, Wijk.Naam as Wijk , [%Criminaliteit].[%Inbraak] as PercentageInbraak
-from [%Criminaliteit]
-join StadsGebied on [%Criminaliteit].GID = StadsGebied.GID
-join Wijk on [%Criminaliteit].WID = Wijk.WID
-where Wijk.Naam = 'Stadsdriehoek'
