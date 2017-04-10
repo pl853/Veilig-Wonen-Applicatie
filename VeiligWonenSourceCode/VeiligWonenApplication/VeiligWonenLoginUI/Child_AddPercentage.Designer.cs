68beb126fb4c0f4c1fa2b38fef45041dad6718ad
Zaahir <zarcevtz@outlook.com>
2017-04-10 13:54:56 +0200

    Test

--- VeiligWonenSourceCode/VeiligWonenApplication/VeiligWonenLoginUI/Child_AddPercentage.Designer.cs
+++ VeiligWonenSourceCode/VeiligWonenApplication/VeiligWonenLoginUI/Child_AddPercentage.Designer.cs
@@ -28,33 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(53, 38);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
             // 
             // Child_AddPercentage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(633, 474);
-            this.Controls.Add(this.label1);
             this.Name = "Child_AddPercentage";
             this.Text = "Child_AddPercentage";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
     }
 }
