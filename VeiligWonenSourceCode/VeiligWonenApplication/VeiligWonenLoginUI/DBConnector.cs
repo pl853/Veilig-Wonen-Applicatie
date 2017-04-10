a7bd0e6f4d8b3cd98651d18469400e8cacd7fdbe
pieterLems <Pieter1212112@gmail.com>
2017-04-10 13:56:03 +0200

    New

--- VeiligWonenSourceCode/VeiligWonenApplication/VeiligWonenLoginUI/DBConnector.cs
+++ VeiligWonenSourceCode/VeiligWonenApplication/VeiligWonenLoginUI/DBConnector.cs
@@ -13,5 +13,6 @@ namespace VeiligWonenLoginUI
         {
             return ConfigurationManager.ConnectionStrings[name].ConnectionString;
         }
+        
     }
 }
--- VeiligWonenSourceCode/VeiligWonenApplication/VeiligWonenLoginUI/Program.cs
+++ VeiligWonenSourceCode/VeiligWonenApplication/VeiligWonenLoginUI/Program.cs
@@ -14,6 +14,12 @@ namespace VeiligWonenLoginUI
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new LoginUI());
+
+            //Connection strings : 
+            //Pieter  :  Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\piete\OneDrive\Documents\Projects\Overig\VeiligWonenApplicatie\VeiligWonenDatabse\VeiligWonenDataBase.mdf;Integrated Security=True;Connect Timeout=30
+            //Suraj : 
+            //Asjad :
+            //Zahir : 
         }
     }
 }
